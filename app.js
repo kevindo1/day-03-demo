f92e69a357430004a9978f13f5d1c351644d80fc
Kevin Do <kevind0501@gmail.com>
2021-09-29 11:05:32 -0700

    tested javascript

--- app.js
+++ app.js
@@ -6,3 +6,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+// console.log('Hello World');
+
+// const PI = 3.14;
+// console.log(PI);
+// console.log(`The area of a circle with radius 2 is: ${PI * 2 * 2}`);
+
+// let name = 'Kevin';
+// let plantName = 'Monstera';
+// console.log(`My name is ${name} and my favorite plant is ${plantName}`);
+
+//arrays 
+// let nums = [1, 2, 3, 4];
+// let names = ['julie', 'benny', 'tis', 'dan', 'casey'];
+// let mixed = [1, 'julie', 2, 'benny'];
+// console.log(nums);
+// console.log(names);
+// console.log(mixed);
+
+let colors = ['red', 'white', 'blue'];
+console.log(colors[0]);
+
+const theColorBlue = colors[2];
+console.log(theColorBlue);
+
+
